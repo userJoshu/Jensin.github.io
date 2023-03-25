0e33c54d0a49a7fa4e4fbb8a21d229f73edb9ae7
userJoshu <marcelljis23@gmail.com>
2023-03-25 23:06:02 +0700

    update-14
    Maps Page
    Maps for laptop

--- Maps/maps.js
+++ Maps/maps.js
@@ -10,3 +10,15 @@ function changeImg(fileName){
     let img = document.querySelector("#imgMapChange");
     img.setAttribute("src", fileName);
 }
+
+function changeDesc(changeMapTitle, changeMapDesc01, changeMapDesc02){
+    var title = document.querySelector('h2');
+    title.innerHTML = changeMapTitle;
+    
+    var desc01 = document.querySelector('#desc01');
+    desc01.innerHTML = changeMapDesc01;
+
+    var desc02 = document.querySelector('#desc02');
+    desc02.innerHTML = changeMapDesc02;
+    
+}
