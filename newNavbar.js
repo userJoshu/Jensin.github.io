5409140f1fc1c693a4245cb2cf8c985bf3f6f65c
userJoshu <marcelljis23@gmail.com>
2023-03-29 11:30:25 +0700

    update-30
    Navbar Done

--- newNavbar.js
+++ newNavbar.js
@@ -2,11 +2,25 @@ const toggleButton = document.querySelector('.toggleButton')
 const toggleButtonLine = document.querySelector('.toggleButton line')
 const dropdown = document.querySelector('.dropdown')
 
+const line1 = document.querySelector('#line1')
+const line2 = document.querySelector('#line2')
+const line3 = document.querySelector('#line3')
+
+
 toggleButton.onclick = function (){
     dropdown.classList.toggle('open')
     const isOpen = dropdown.classList.contains('open')
 
-    toggleButtonLine.classList = isOpen 
-        
+    // toggleButtonLine.classList = isOpen 
+
 
 }
+
+toggleButton.addEventListener("click", () => {
+    line1.classList.toggle("animationLine1");
+
+    line2.classList.toggle("animationLine2");
+
+    line3.classList.toggle("animationLine3");
+})
+
